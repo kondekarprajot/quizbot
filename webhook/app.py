d4c29bb1b402a3190ace577a2a2f66fb6c5c6c44
kondekarprajot <kondekarprajot@gmail.com>
2025-04-22 10:37:48 +0530

    Update app.py

--- webhook/app.py
+++ webhook/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Store the current question and answer for each session
 sessions = {}
